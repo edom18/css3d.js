3c63410ea02a3ce6f0333eae49ab695f93612ed1
edom18 <edo.m18@gmail.com>
2013-06-18 15:07:54 +0900

    updated

--- js/css3d.js
+++ js/css3d.js
@@ -2,32 +2,256 @@
 
     'use strict';
 
-    var Camera;
-
     win.Float32Array = win.Float32Array || win.Array;
+
+    var prop_names = (function () {
+        var ret = {};
+        return ret;
+    }());
+
+    var Css3D = Class.extend({
+        init: function () {
+            this.parent = null;
+            this.children = [];
+            this.vertices = [];
+            this.position = new Vector3();
+            this.rotation = new Vector3();
+            this.scale = new Vector3(1, 1, 1);
+            this.up    = new Vector3(0, 1, 0);
+
+            this.matrixScale = new Matrix4();
+            this.matrixTranslate = new Matrix4();
+            this.matrixRotation = new Matrix4();
+            this.matrix = new Matrix4();
+            this.matrixWorld = new Matrix4();
+
+            this.updateMatrix();
+        },
+
+        updateScale: (function () {
+
+            var sm = new Matrix4();
+
+            return function () {
+                if (this.prevScale && this.scale.equal(this.prevScale)) {
+                    return false;
+                }
+
+                this.prevScale = this.scale.clone();
+                this.matrixScale = sm.clone().scale(this.scale);
+
+                return true;
+            };
+        }()),
+
+        updateTranslate: (function () {
+
+            var tm = new Matrix4();
+
+            return function () {
+                if (this.prevPosition && this.position.equal(this.prevPosition)) {
+                    return false;
+                }
+
+                this.prevPosition = this.position.clone();
+                this.matrixTranslate = tm.clone().translate(this.position);
+
+                return true;
+            };
+        }()),
+
+        updateRotation: (function () {
+
+            var rmx = new Matrix4(),
+                rmy = new Matrix4(),
+                rmz = new Matrix4();
+
+            return function () {
+
+                if (this.prevRotation && this.rotation.equal(this.prevRotation)) {
+                    return false;
+                }
+
+                x = this.rotation.x * DEG_TO_RAD;
+                y = this.rotation.y * DEG_TO_RAD;
+                z = this.rotation.z * DEG_TO_RAD;
+
+                var tmp = new Matrix4();
+                rmx.rotationX(x);
+                rmy.rotationY(y);
+                rmz.rotationZ(z);
+
+                tmp.multiplyMatrices(rmx, rmy);
+                tmp.multiply(rmz);
+
+                this.prevRotation = this.rotation.clone();
+                this.matrixRotation = tmp;
+
+                return true;
+            };
+        }()),
+
+        updateMatrix: function () {
+
+            var updatedScale     = this.updateScale(),
+                updatedRotation  = this.updateRotation(),
+                updatedTranslate = this.updateTranslate();
+
+            if (updatedRotation || updatedTranslate || updatedScale) {
+                this.matrix.multiplyMatrices(this.matrixTranslate, this.matrixRotation);
+                this.matrix.multiply(this.matrixScale);
+                this.needUpdateMatrix = true;
+            }
+            else {
+                this.needUpdateMatrix = false;
+            }
+
+            var children = this.children;
+            for (var i = 0, c, l = children; i < l; i++) {
+                c = children[i];
+                c.updateMatrix();
+            }
+        },
+
+        updateMatrixWorld: function (force) {
+
+            if (!this.parent) {
+                this.matrixWorld.copy(this.matrix);
+            }
+            else {
+                if (force || this.parent.needUpdateMatrix || this.needUpdateMatrix || this.parent.needUpdateMatrixWorld) {
+                    this.matrixWorld.multiplyMatrices(this.parent.matrixWorld, this.matrix);
+                    this.needUpdateMatrixWorld = true;
+                }
+                else {
+                    this.needUpdateMatrixWorld = false
+                }
+            }
+
+            var children = this.children;
+            for (var i = 0, c, l = children; i < l; i++) {
+                c = children[i];
+                c.updateMatrixWorld();
+            }
+        },
+
+        getVerticesByProjectionMatrix: function (m) {
+
+            var ret = [],
+                wm,
+                tmp,
+                outside,
+                vertices = this.vertices;
+
+            for (var i = 0, v, l = vertices.length; i < l; i++) {
+                v = vertices[i];
+                wm = Matrix4.multiply(m, this.matrixWorld);
+                tmp = [];
+                outside = v.clone().applyProjection(wm, tmp);
+
+                if (!outside) {
+                    continue;
+                }
+
+                ret = ret.concat(tmp[0].toArray().concat(tmp[1]));
+            }
+
+            return ret;
+        },
+
+        add: function (object) {
+             if (this === object) {
+                 return null;
+             }
+
+            object.parent && object.parent.remove(object);
+
+            this.children.push(object);
+            object.parent = this;
+        },
+
+        remove: function (object) {
+            if (this === object) {
+                return null;
+            }
+
+            index = this.children.indexOf(object);
+
+            if (index === -1) {
+                return null;
+            }
+
+            return this.children.splice(index, 1);
+        }
+    });
+
+    /**
+     * Renderer class
+     * @class
+     * @constructor
+     */
+    var Renderer = Class.extend({
+        render: function (camera, scene) {
+        
+        }
+    });
+
+
+    var Scene = Class.extend({
+        init: function () {
+            this.materials = [];
+        },
+
+        add: function (material) {
+            if (material instanceof Css3D) {
+                this.materials.push(material);
+            }
+        },
+
+        update: function () {
+            var materials = this.materials;
+            for (var i = 0, m, l = materials; i < l; i++) {
+                m = materials[i];
+                m.updateMatrix()
+                m.updateMatrixWorld()
+            }
+        }
+    });
 
     /**
       Camera class
       @constructor
-      @param {number} fov Field of view.
-      @param {number} aspect Aspect ratio.
-      @param {number} near Near clip.
-      @param {number} far far clip.
+      @param {number} perspective
       @param {Vector3} position Position vector.
       */
-    Camera = Class.extend({
-        init: function (fov, aspect, near, far, position) {
-            this.fov = fov;
-            this.aspect = aspect;
-            this.near = near;
-            this.far = far;
-            this.position = position != null ? position : new Vector3(0, 0, 20);
+    var Camera = Class.extend({
+        defaults: {
+            perspective: 300,
+            position: new Vector3(0, 0, 20)
+        },
+        init: function (args) {
+            args || (args = {});
 
-            this._super.apply(this, arguments);
+            this.perspective = args.perspective || this.defaults.perspective;
+            this.position = args.position || this.defaults.position;
 
-            this.lookAtMatrix = new Matrix4;
-            this.viewMatrix = new Matrix4;
-            this.projectionMatrix = new Matrix4;
+            this.lookAtMatrix     = new Matrix4();
+            this.viewMatrix       = new Matrix4();
+            this.projectionMatrix = new Matrix4();
+
+            this.viewport = args.viewport || this._createElement();
+            this._setup();
+        },
+
+        _setup: function () {
+            this.viewport.style.webkitPerspective = this.perspective + 'px';
+            this.viewport.style.webkitTransformStyle = 'preserve-3d';
+        },
+
+        _createElement: function () {
+            var viewport = doc.createElement('div');
+            viewport.className = 'stage';
+            return viewport;
         },
 
         getProjectionMatrix: function() {
@@ -40,9 +264,10 @@
         },
 
         updateLookAt: (function() {
-            var lm, previous;
-            lm = new Matrix4;
-            previous = null;
+
+            var lm = new Matrix4(),
+                previous = null;
+
             return function() {
                 if (!previous) {
                     previous = this.position.clone();
@@ -53,16 +278,19 @@
                 if (!this.lookAtLock) {
                     this.target.add(this.position.clone().sub(previous));
                 }
+
                 this.lookAt();
-                return previous = this.position.clone();
+
+                return (previous = this.position.clone());
             };
         })(),
 
         lookAt: (function() {
-            var m1;
-            m1 = new Matrix4;
+
+            var m1 = new Matrix4();
+
             return function(target) {
-                this.target = target || this.target || new Vector3;
+                this.target = target || this.target || new Vector3();
                 m1.lookAt(this.position, this.target, this.up);
                 return this.viewMatrix.copy(m1);
             };
@@ -73,7 +301,7 @@
     /*! -------------------------------------------------------------
         EXPORTS.
     ----------------------------------------------------------------- */
-    exports.Matrix4 = Matrix4;
-    exports.Camera  = Camera;
+    exports.Camera   = Camera;
+    exports.Renderer = Renderer;
 
 }(window, window.document, window.Class, window));
--- js/main.js
+++ js/main.js
@@ -2,4 +2,15 @@
 
     'use strict';
 
+    var viewport = doc.getElementById('viewport');
+    var renderer = new Renderer({
+        viewport: viewport
+    });
+
+    var camera = new Camera({
+        perspective: 300,
+        viewport: viewport,
+        position: new Vector3(0, 0, 100)
+    });
+
 })(window, window.document);
